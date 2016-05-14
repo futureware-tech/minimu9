f38117215a6f85f1504e82f6d2931faafe714def
Artem Sheremet <dot.doom@gmail.com>
2016-05-14 19:20:29 +0200

    Add documentation for minimu9.Vector

--- vector.go
+++ vector.go
@@ -2,10 +2,12 @@ package minimu9
 
 import "github.com/golang/geo/r3"
 
+// 3D Vector with integer dimensions.
 type Vector struct {
 	X, Y, Z int16
 }
 
+// ToR3 converts the vector to r3.Vector, which has useful methods.
 func (v *Vector) ToR3() *r3.Vector {
 	return &r3.Vector{
 		X: float64(v.X),
