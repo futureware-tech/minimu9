b269294a503e043041d81aa1f53724c323421549
Artem Sheremet <dot.doom@gmail.com>
2016-05-17 08:17:03 +0200

    l3gd: remove frequency etc, use optimized values

--- l3gd/l3gd.go
+++ l3gd/l3gd.go
@@ -1,11 +1,9 @@
 package l3gd
 
 import (
-	"bytes"
-	"encoding/binary"
-
 	"github.com/dasfoo/i2c"
 	"github.com/dasfoo/minimu9"
+	"github.com/golang/geo/r3"
 )
 
 // Gyro is a sensor driver implementation for L3GD20H Gyro.
@@ -35,78 +33,27 @@ const (
 
 // Sleep puts the sensor in low power consumption mode.
 func (l3g *Gyro) Sleep() error {
-	// There's not just power control in CTRL1, we need to keep other values.
-	var bw byte
-	var err error
-	if bw, err = l3g.bus.ReadByteFromReg(l3g.address, regCtrl1); err != nil {
-		return err
-	}
 	// We are actually setting it to power-down mode rather than sleep.
 	// Power-down consumes less power, but takes longer to wake.
-	return l3g.bus.WriteByteToReg(l3g.address, regCtrl1, bw&^(1<<3))
+	return l3g.bus.WriteByteToReg(l3g.address, regCtrl1, 0x00)
 }
 
 // Wake enables sensor if it was put into power-down mode with Sleep().
 func (l3g *Gyro) Wake() error {
-	var bw byte
-	var err error
-	if bw, err = l3g.bus.ReadByteFromReg(l3g.address, regCtrl1); err != nil {
+	// 200 Hz Output Data Rate, 50 Hz Bandwidth, normal mode with all 3 axes enabled.
+	if err := l3g.bus.WriteByteToReg(l3g.address, regCtrl1, 0x6F); err != nil {
 		return err
 	}
-	return l3g.bus.WriteByteToReg(l3g.address, regCtrl1, bw|0xf0)
-}
-
-var bitsLowodrDrForFrequency = [...][3]int{
-	// There's also "bandwidth" in that register, but experimentally
-	// proven useless for I2C. Perhaps only useful for SPI.
-	{12, 1, 0x0f},  // 10
-	{25, 1, 0x4f},  // 20
-	{50, 1, 0x8f},  // 40
-	{100, 0, 0x0f}, // 80
-	{200, 0, 0x4f}, // 150
-	{400, 0, 0x8f}, // 300
-	{800, 0, 0xcf}, // 600
-}
-
-// SetFrequency sets Output Data Rate, in Hz, range 12 .. 800.
-func (l3g *Gyro) SetFrequency(hz int) error {
-	// ~250 dps full scale (gain).
+	// +/- 250 dps (degrees per second) scale range.
 	if err := l3g.bus.WriteByteToReg(l3g.address, regCtrl4, 0x00); err != nil {
 		return err
 	}
-	for i := 0; i < len(bitsLowodrDrForFrequency); i++ {
-		if bitsLowodrDrForFrequency[i][0] >= hz || i == len(bitsLowodrDrForFrequency)-1 {
-			if err := l3g.bus.WriteByteToReg(l3g.address, regLowOdr,
-				byte(bitsLowodrDrForFrequency[i][1])); err != nil {
-				return err
-			}
-			return l3g.bus.WriteByteToReg(l3g.address, regCtrl1,
-				byte(bitsLowodrDrForFrequency[i][2]))
-		}
-	}
-	// This should never happen.
-	return nil
+	// Disable Low Output Data Rate.
+	return l3g.bus.WriteByteToReg(l3g.address, regLowOdr, 0x00)
 }
 
 // Read reads new data from the sensor.
 // Note: err might be a warning about data "freshness" if it's minimu9.DataAvailabilityError.
-// Call sequence:
-//   SetFrequency(...)
-//   in a loop: Read()
-func (l3g *Gyro) Read() (v *minimu9.Vector, err error) {
-	data := make([]byte, 7)
-	if _, err = l3g.bus.ReadSliceFromReg(l3g.address, 0x27|(1<<7), data); err != nil {
-		return
-	}
-	dataReader := bytes.NewReader(data[1:])
-	v = &minimu9.Vector{}
-	if err = binary.Read(dataReader, binary.LittleEndian, v); err != nil {
-		return
-	}
-	if data[0]&0xf0 > 0 {
-		err = &minimu9.DataAvailabilityError{NewDataWasOverwritten: true}
-	} else if data[0]&0x0f == 0 {
-		err = &minimu9.DataAvailabilityError{NewDataNotAvailable: true}
-	}
-	return
+func (l3g *Gyro) Read() (v *r3.Vector, err error) {
+	return minimu9.ReadStatusAndVector(l3g.bus, l3g.address, 0x27)
 }
--- vector.go
+++ vector.go
@@ -1,17 +1,38 @@
 package minimu9
 
-import "github.com/golang/geo/r3"
+import (
+	"bytes"
+	"encoding/binary"
 
-// Vector holds integer dimensions of a 3D vector.
-type Vector struct {
+	"github.com/dasfoo/i2c"
+	"github.com/golang/geo/r3"
+)
+
+type intVector struct {
 	X, Y, Z int16
 }
 
-// ToR3 converts the vector to r3.Vector, which has useful methods.
-func (v *Vector) ToR3() *r3.Vector {
-	return &r3.Vector{
-		X: float64(v.X),
-		Y: float64(v.Y),
-		Z: float64(v.Z),
+// ReadStatusAndVector reads status byte, and 3x2-byte X, Y and Z int16 vector values.
+func ReadStatusAndVector(bus *i2c.Bus, addr, reg byte) (v *r3.Vector, e error) {
+	data := make([]byte, 7)
+	// Set MSB for the slave to advance the register on every read.
+	if _, e = bus.ReadSliceFromReg(addr, reg|(1<<7), data); e != nil {
+		return
 	}
+	dataReader := bytes.NewReader(data[1:])
+	var iv intVector
+	if e = binary.Read(dataReader, binary.LittleEndian, &iv); e != nil {
+		return
+	}
+	if data[0]&0xf0 > 0 {
+		e = &DataAvailabilityError{NewDataWasOverwritten: true}
+	} else if data[0]&0x0f == 0 {
+		e = &DataAvailabilityError{NewDataNotAvailable: true}
+	}
+	v = &r3.Vector{
+		X: float64(iv.X),
+		Y: float64(iv.Y),
+		Z: float64(iv.Z),
+	}
+	return
 }
