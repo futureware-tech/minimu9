e3736dc0c4be984056527575888df4a3f3b498f4
Artem Sheremet <dot.doom@gmail.com>
2016-05-31 13:41:05 +0200

    Update code comments

--- vector.go
+++ vector.go
@@ -8,6 +8,8 @@ import (
 	"github.com/golang/geo/r3"
 )
 
+// IntVector is a 3D vector holding the dimensions in 2-byte signed integers,
+// as all the hardware parts of miniIMU9 do.
 type IntVector struct {
 	X, Y, Z int16
 }
@@ -35,19 +37,18 @@ func ReadStatusAndVector(bus i2c.Bus, addr, reg byte) (v r3.Vector, e error) {
 	return
 }
 
-// ReadVector reads 3x2-byte X, Y and Z int16 vector values.
+// ReadVector reads IntVector dimensions.
 func ReadVector(bus i2c.Bus, addr, reg byte) (v IntVector, e error) {
 	data := make([]byte, 6)
 	// Set MSB for the slave to advance the register on every read.
 	if _, e = bus.ReadSliceFromReg(addr, reg|(1<<7), data); e != nil {
 		return
 	}
-	dataReader := bytes.NewReader(data)
-	e = binary.Read(dataReader, binary.LittleEndian, &v)
+	e = binary.Read(bytes.NewReader(data), binary.LittleEndian, &v)
 	return
 }
 
-// WriteVector writes 3x2-byte X, Y and Z int16 vector values.
+// WriteVector writes IntVector dimensions.
 func WriteVector(bus i2c.Bus, addr, reg byte, v IntVector) error {
 	var data bytes.Buffer
 	if e := binary.Write(&data, binary.LittleEndian, &v); e != nil {
