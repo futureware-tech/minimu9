c962e99f9a887864b929f738a29d6f9625f4ba70
Artem Sheremet <dot.doom@gmail.com>
2016-05-31 09:37:02 +0200

    Return non-pointer r3.Vector and add Calibration

--- lsm303d/lsm303d.go
+++ lsm303d/lsm303d.go
@@ -1,6 +1,8 @@
 package lsm303d
 
 import (
+	"math"
+
 	"github.com/dasfoo/i2c"
 	"github.com/dasfoo/minimu9"
 	"github.com/golang/geo/r3"
@@ -61,14 +63,18 @@ func (a *Accelerometer) Wake() error {
 	if err := a.bus.WriteByteToReg(a.address, regCtrl1, 0x57); err != nil {
 		return err
 	}
-	// +/- 2g full scale.
-	return a.bus.WriteByteToReg(a.address, regCtrl2, 0x00)
+	// TODO(dotdoom): write to regCtrl7, it controls accel as well.
+	// +/- 8g full scale.
+	return a.bus.WriteByteToReg(a.address, regCtrl2, 0x18)
 }
 
-// Read reads new data from the accelerometer.
+// Read reads acceleration vector, in g, from the accelerometer.
 // Note: err might be a warning about data "freshness" if it's minimu9.DataAvailabilityError.
-func (a *Accelerometer) Read() (*r3.Vector, error) {
-	return minimu9.ReadStatusAndVector(a.bus, a.address, 0x27)
+func (a *Accelerometer) Read() (r3.Vector, error) {
+	v, e := minimu9.ReadStatusAndVector(a.bus, a.address, 0x27)
+	// +-8 is the full scale
+	// /!\ BUG /!\ maxInt16+1
+	return v.Mul(1.0 / (math.MaxInt16 / 8)), e
 }
 
 // Wake returns magnetometer into operating mode.
@@ -90,8 +96,70 @@ func (m *Magnetometer) Sleep() error {
 	return m.bus.WriteByteToReg(m.address, regCtrl7, 1<<1)
 }
 
+func pushMinMax(v minimu9.IntVector, min, max *minimu9.IntVector) {
+	if v.X < min.X {
+		min.X = v.X
+	} else if v.X > max.X {
+		max.X = v.X
+	}
+	if v.Y < min.Y {
+		min.Y = v.Y
+	} else if v.Y > max.Y {
+		max.Y = v.Y
+	}
+	if v.Z < min.Z {
+		min.Z = v.Z
+	} else if v.Z > max.Z {
+		max.Z = v.Z
+	}
+}
+
+// Calibrate measures magnetometer bias until stop channel is written to.
+// Calibration offset is then saved to the magnetometer and returned.
+func (m *Magnetometer) Calibrate(stop chan int) (r3.Vector, error) {
+	// Reset any previously computed offset.
+	if e := minimu9.WriteVector(m.bus, m.address, 0x16, minimu9.IntVector{}); e != nil {
+		return r3.Vector{}, e
+	}
+	min := minimu9.IntVector{X: math.MaxInt16, Y: math.MaxInt16, Z: math.MaxInt16}
+	max := minimu9.IntVector{X: math.MinInt16, Y: math.MinInt16, Z: math.MinInt16}
+	for {
+		select {
+		case <-stop:
+			avg := minimu9.IntVector{
+				X: int16((int(min.X) + int(max.X)) >> 1),
+				Y: int16((int(min.Y) + int(max.Y)) >> 1),
+				Z: int16((int(min.Z) + int(max.Z)) >> 1),
+			}
+			// TODO(dotdoom): scale the vector on return.
+			return r3.Vector{
+					X: float64(avg.X),
+					Y: float64(avg.Y),
+					// FIXME(dotdoom): maxint16+1
+					Z: float64(avg.Z)}.Mul(1.0 / (math.MaxInt16 / 4)),
+				minimu9.WriteVector(m.bus, m.address, 0x16, avg)
+		default:
+			v, e := minimu9.ReadVector(m.bus, m.address, 0x08)
+			if e != nil {
+				return r3.Vector{}, e
+			}
+			pushMinMax(v, &min, &max)
+		}
+	}
+}
+
 // Read reads new data from the magnetometer.
 // Note: err might be a warning about data "freshness" if it's minimu9.DataAvailabilityError.
-func (m *Magnetometer) Read() (*r3.Vector, error) {
-	return minimu9.ReadStatusAndVector(m.bus, m.address, 0x07)
+func (m *Magnetometer) Read() (r3.Vector, error) {
+	v, e := minimu9.ReadStatusAndVector(m.bus, m.address, 0x07)
+	// +-4 is the full scale set above
+	// FIXME(dotdoom): maxint16+1
+	return v.Mul(1.0 / (math.MaxInt16 / 4)), e
+}
+
+// RelativeHeading returns current heading, in radians. May only be used in relative computations.
+func RelativeHeading(a *Accelerometer, m *Magnetometer) (float64, error) {
+	_ = a
+	v, e := m.Read()
+	return math.Atan2(v.Y, v.X), e
 }
--- vector.go
+++ vector.go
@@ -8,31 +8,52 @@ import (
 	"github.com/golang/geo/r3"
 )
 
-type intVector struct {
+type IntVector struct {
 	X, Y, Z int16
 }
 
 // ReadStatusAndVector reads status byte, and 3x2-byte X, Y and Z int16 vector values.
-func ReadStatusAndVector(bus *i2c.Bus, addr, reg byte) (v *r3.Vector, e error) {
-	data := make([]byte, 7)
-	// Set MSB for the slave to advance the register on every read.
-	if _, e = bus.ReadSliceFromReg(addr, reg|(1<<7), data); e != nil {
+func ReadStatusAndVector(bus i2c.Bus, addr, reg byte) (v r3.Vector, e error) {
+	var status byte
+	if status, e = bus.ReadByteFromReg(addr, reg); e != nil {
 		return
 	}
-	dataReader := bytes.NewReader(data[1:])
-	var iv intVector
-	if e = binary.Read(dataReader, binary.LittleEndian, &iv); e != nil {
+	var iv IntVector
+	if iv, e = ReadVector(bus, addr, reg+1); e != nil {
 		return
 	}
-	if data[0]&0xf0 > 0 {
+	if status&0xf0 > 0 {
 		e = &DataAvailabilityError{NewDataWasOverwritten: true}
-	} else if data[0]&0x0f == 0 {
+	} else if status&0x0f == 0 {
 		e = &DataAvailabilityError{NewDataNotAvailable: true}
 	}
-	v = &r3.Vector{
+	v = r3.Vector{
 		X: float64(iv.X),
 		Y: float64(iv.Y),
 		Z: float64(iv.Z),
 	}
 	return
+}
+
+// ReadVector reads 3x2-byte X, Y and Z int16 vector values.
+func ReadVector(bus i2c.Bus, addr, reg byte) (v IntVector, e error) {
+	data := make([]byte, 6)
+	// Set MSB for the slave to advance the register on every read.
+	if _, e = bus.ReadSliceFromReg(addr, reg|(1<<7), data); e != nil {
+		return
+	}
+	dataReader := bytes.NewReader(data)
+	e = binary.Read(dataReader, binary.LittleEndian, &v)
+	return
+}
+
+// WriteVector writes 3x2-byte X, Y and Z int16 vector values.
+func WriteVector(bus i2c.Bus, addr, reg byte, v IntVector) error {
+	var data bytes.Buffer
+	if e := binary.Write(&data, binary.LittleEndian, &v); e != nil {
+		return e
+	}
+	// Set MSB for the slave to advance the register on every read.
+	_, e := bus.WriteSliceToReg(addr, reg|(1<<7), data.Bytes())
+	return e
 }
